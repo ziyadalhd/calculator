4b1f4ab46c3395c500d41c3dd93ab71ef1849804
ziyad <ziyadalhdriti@gmail.com>
2024-06-21 22:25:21 +0300

    buttons using grids and collor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,25 @@
     </title>
 </svelte:head>
 
-<div class="bg-[rgb(254,254,254)] h-[30rem] w-[20rem] rounded-3xl flex justify-center items-center">
-   
+<div class="bg-[rgb(254,254,254)] min-h-[25rem] w-[18rem] rounded-3xl grid grid-cols-4 gap-1">
+    <button class="bg-[#f3f6fd]">%</button>
+    <button class="bg-[#f3f6fd]">√</button>
+    <button class="bg-[#f3f6fd]">CE</button>
+    <button class="bg-[#1f2b55] text-[#f6f8f9]">C</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button class="bg-[#fe4459] text-[#f6f8f9]">-</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button class="bg-[#1e88fc] text-[#f6f8f9]">÷</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button class="bg-[#fdc704] text-[#f6f8f9]">×</button>
+    <button>.</button>
+    <button>0</button>
+    <button>=</button>
+    <button class="bg-[#63dd75] text-[#f6f8f9]">+</button>
 </div>
